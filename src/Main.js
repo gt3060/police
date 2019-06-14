e56762b8ba9d05a2bf03035e3083accfd7292a1a
znanjie <1016224076@qq.com>
2019-06-14 11:32:56 +0800

    场景更新

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,37 @@
 ﻿import GameConfig from "./GameConfig";
 import ui from "./ui/layaMaxUI";
+
 class Main {
 	constructor() {
 		//根据IDE设置初始化引擎		
-		if (window["Laya3D"]) Laya3D.init(GameConfig.width, GameConfig.height);
-		else Laya.init(GameConfig.width, GameConfig.height, Laya["WebGL"]);
+		if (window["Laya3D"]) {
+			Laya3D.init(GameConfig.width, GameConfig.height);
+		} else {
+			Laya.init(GameConfig.width, GameConfig.height, Laya["WebGL"]);
+		}
 		Laya["Physics"] && Laya["Physics"].enable();
 		Laya["DebugPanel"] && Laya["DebugPanel"].enable();
+
+		// 舞台配置
 		Laya.stage.scaleMode = GameConfig.scaleMode;
 		Laya.stage.screenMode = GameConfig.screenMode;
 		Laya.stage.alignV = GameConfig.alignV;
 		Laya.stage.alignH = GameConfig.alignH;
+
 		//兼容微信不支持加载scene后缀场景
 		Laya.URL.exportSceneToJson = GameConfig.exportSceneToJson;
 
 		//打开调试面板（通过IDE设置调试模式，或者url地址增加debug=true参数，均可打开调试面板）
-		if (GameConfig.debug || Laya.Utils.getQueryString("debug") == "true") Laya.enableDebugPanel();
-		if (GameConfig.physicsDebug && Laya["PhysicsDebugDraw"]) Laya["PhysicsDebugDraw"].enable();
-		if (GameConfig.stat) Laya.Stat.show();
+		if (GameConfig.debug || Laya.Utils.getQueryString("debug") == "true") {
+			Laya.enableDebugPanel();
+		}
+		if (GameConfig.physicsDebug && Laya["PhysicsDebugDraw"]) {
+			Laya["PhysicsDebugDraw"].enable();
+		}
+		if (GameConfig.stat) {
+			Laya.Stat.show();
+		}
+		// 错误提示弹窗
 		Laya.alertGlobalError = true;
 
 		//激活资源版本控制，version.json由IDE发布功能自动生成，如果没有也不影响后续流程
