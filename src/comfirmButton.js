8cc5c34b99be6d36298d0bbcebd6c6b3a4ee4dd9
gooff327 <gooff1997@outlook.com>
2019-06-15 10:13:00 +0800

    add task scene to src ,create buttons script file & move texture to assets

---
+++ src/comfirmButton.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+let Events = Laya.Event
+/**
+ * 确认出警按钮脚本，点击后进行场景的跳转
+ */
+    export default class ComfirmButton extends Button{
+    constructor(){
+        super()
+    }
+
+}
